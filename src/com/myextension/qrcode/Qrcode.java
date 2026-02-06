048b2e598129ee5b4a7dca1a5b3137c9c464195b
amar3219 <142188551+amar3219@users.noreply.github.com>
2026-02-06 12:52:14 +0530

    Update Qrcode.java

--- src/com/myextension/qrcode/Qrcode.java
+++ src/com/myextension/qrcode/Qrcode.java
@@ -1,22 +1,83 @@
-name: Build AIX
-on: [push, workflow_dispatch]
-jobs:
-  build:
-    runs-on: ubuntu-latest
-    steps:
-      - uses: actions/checkout@v3
-      - name: Setup Java
-        uses: actions/setup-java@v3
-        with:
-          distribution: 'temurin'
-          java-version: '17'
-      - name: Build with Rush
-        run: |
-          curl -fsSL https://raw.githubusercontent.com/shreyashsaitwal/rush-cli/main/scripts/install/install.sh | bash
-          /home/runner/.rush/bin/rush build
-      - name: Upload Result
-        uses: actions/upload-artifact@v3
-        with:
-          name: MyExtensionFile
-          path: "out/*.aix"
-  
+package com.myextension.qrcode;
+
+import com.google.appinventor.components.annotations.*;
+import com.google.appinventor.components.common.ComponentCategory;
+import com.google.appinventor.components.runtime.*;
+import android.webkit.WebView;
+import android.webkit.ValueCallback;
+
+@DesignerComponent(version = 1,
+    description = "Extension for Live Variable Monitoring & URL Control",
+    category = ComponentCategory.EXTENSION,
+    nonVisible = true)
+@SimpleObject(external = true)
+public class Qrcode extends AndroidNonvisibleComponent {
+
+    private WebView web;
+
+    public Qrcode(ComponentContainer container) {
+        super(container.$form());
+    }
+
+    // WebViewer ko connect karein
+    @SimpleProperty(description = "Connect existing WebViewer to this extension")
+    public void WebViewerComponent(WebViewer webViewer) {
+        this.web = (WebView) webViewer.getView();
+    }
+
+    // URL set aur load karein
+    @SimpleFunction(description = "Load a specific URL in the connected WebViewer")
+    public void SetGameURL(String url) {
+        if (this.web != null) {
+            this.web.loadUrl(url);
+        }
+    }
+
+    // Saare variables ki list nikalne ke liye
+    @SimpleFunction(description = "Fetches all global variables from the game")
+    public void GetVariableList() {
+        if (this.web != null) {
+            final String script = "Object.keys(window).filter(k => typeof window[k] !== 'function').join(',');";
+            this.web.post(new Runnable() {
+                @Override
+                public void run() {
+                    web.evaluateJavascript(script, new ValueCallback<String>() {
+                        @Override
+                        public void onReceiveValue(String value) {
+                            OnListReceived(value.replace("\"", "")); 
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    // Kisi variable ki live value lene ke liye
+    @SimpleFunction(description = "Get the live value of a variable by its name")
+    public void GetValueByName(final String varName) {
+        if (this.web != null) {
+            final String script = "window['" + varName + "']";
+            this.web.post(new Runnable() {
+                @Override
+                public void run() {
+                    web.evaluateJavascript(script, new ValueCallback<String>() {
+                        @Override
+                        public void onReceiveValue(String value) {
+                            OnValueReceived(varName, value.replace("\"", ""));
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    @SimpleEvent(description = "Triggered when variable list is fetched")
+    public void OnListReceived(String list) {
+        EventDispatcher.dispatchEvent(this, "OnListReceived", list);
+    }
+
+    @SimpleEvent(description = "Triggered when a specific variable value is received")
+    public void OnValueReceived(String name, String value) {
+        EventDispatcher.dispatchEvent(this, "OnValueReceived", name, value);
+    }
+}
